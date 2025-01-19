8093550f77fc460240c4bff18609051c09ee5a5c
gongym <1967988842@qq.com>
2025-01-19 12:29:42 +0800

    update

---
+++ update.bat
@@ -0,0 +1,21 @@
+call git clone --depth 1 https://github.com/whuanle/lsm
+call git clone --depth 1 https://github.com/skyzh/mini-lsm
+call git clone --depth 1 https://github.com/lotusdblabs/lotusdb
+call git clone --depth 1 https://github.com/rosedblabs/rosedb
+call git clone --depth 1 https://github.com/facebook/rocksdb
+call git clone --depth 1 https://github.com/dgraph-io/badger
+call git clone --depth 1 https://github.com/chaisql/chai
+call git clone --depth 1 https://github.com/cockroachdb/pebble
+call git clone --depth 1 https://github.com/etcd-io/bbolt
+call git clone --depth 1 https://github.com/syndtr/goleveldb
+call git clone --depth 1 https://github.com/auxten/go-sqldb
+call git clone --depth 1 https://github.com/chenjiandongx/mandodb
+call git clone --depth 1 https://github.com/kebukeYi/MVCC
+call git clone --depth 1 https://github.com/nananatsu/simple-raft
+call git clone --depth 1 https://github.com/jojo-nica/MYDB
+call git clone --depth 1 https://github.com/qw4990/NYADB2
+
+call git add .
+call git commit -m "update"
+
+@pause
